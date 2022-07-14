e09c538dff1827608ebeae13fa0e2af3cdfde0d3
Tyson Lukale <47857498+LukaleTyson@users.noreply.github.com>
2022-07-14 10:39:30 +0300

    Update UnderWork.jsx
    Added link on  button the underwork page

--- src/pages/UnderWork.jsx
+++ src/pages/UnderWork.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import {Link} from "react-router-dom"
 
 const Wrapper = styled.div`
     flex:4;
@@ -26,7 +27,9 @@ const UnderWork = () => {
       <Wrapper>
           <Container>
               <h2>Working on this feature!!</h2>
-              <Button>Home page</Button>
+                <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>>
+                    <Button>Home page</Button>
+                </Link>
           </Container>
     </Wrapper>
   )
